f10b2945c9c9813b25ac2133ea48e1e1bb4439d5
Pedro Rosario <pdrorosa18912@gmail.com>
2020-07-13 23:53:31 -0300

    Aula 10 e Aula 11

---
+++ Aula 11/ex008.js
@@ -0,0 +1,10 @@
+console.log("O console funcionou")
+
+var vel = 87
+console.log(`A velocidade do seu carro é ${vel}Km/H`)
+if(vel > 60){
+    console.log('Você ultrapassou o limete de velocidade')
+}else{
+    console.log('Dirija sempre usando sinto de segurança')
+}
+
